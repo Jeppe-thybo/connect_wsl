7e5ebc74e685407d507d6b36b20d0d51090ecbd1
Jeppe-thybo <Jeppethybo222@gmail.com>
2025-12-15 10:44:42 +0100

    1

--- SIDR.sh
+++ SIDR.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=create_data
-#SBATCH --partition=q24,q28,q36,q40,q48
+#SBATCH --job-name=SIDR
+#SBATCH --partition=qany
 #SBATCH --mem-per-cpu=2g
 #SBATCH --ntasks=200
 #SBATCH --cpus-per-task=1
-#SBATCH --time=08:00:00
-#SBATCH --output=test.out
+#SBATCH --time=04:00:00
+#SBATCH --output=SIDR.out
 echo "========= Job started at `date` =========="
 
 # activate proper environment if needed
-source activate ConnectEnvironment
 module load gcc openmpi
 
 # source planck data (load path from connect.conf)
@@ -18,6 +17,6 @@ path_split=(${clik_line//= / })
 path="$(echo ${path_split[1]} | sed "s/'//g")bin/clik_profile.sh"
 source $path
 
-python connect.py create input/example.param
+python connect.py create input/SIDR.param
 
 echo "========= Job finished at `date` =========="
